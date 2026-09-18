1173112326fe22232797f063a5da1d42587464a4
kuyaEJ <69058410+kuyaEJ@users.noreply.github.com>
2026-09-17 20:02:33 -0700

    completed React project for learning React props and React components

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,127 @@
+import '../index.css'
+import Event from './Event.jsx'
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='time'>8 am</td>
+                        <Event event='Library Browsing 📖' color='green' location='Los Angeles'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>9 am</td>
+                        <Event event='Mall Shopping 🛍️' color='green' location='Orange County'/>
+                        <Event event='Work' color='red' location='Long Beach'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>10 am</td>
+                        <Event event='Lunch Break 🌯' color='blue' location='Los Angeles'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>12 pm</td>
+                        <td></td>
+                        <Event event='Lunch Break 🍜' color='blue' location='Silverlake' />
+                        <td></td>
+                        <td></td>
+                        <Event event='Gym 👟' color='blue' location='Home'/>
+                        <td></td>
+                        <Event event='Church ⛪️' color='blue' location='Beverly'/>
+                    </tr>
+                    <tr>
+                        <td className='time'>1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <Event event='Groceries 🛒' color='red' location='Los Angeles'/>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className='time'>5 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+};
+
+export default Calendar;
